5cf101c4b2ba7b020cf835e9c9d87b5f24c6ec3e
Michael Fladischer <FladischerMichael@fladi.at>
2017-07-12 13:09:05 +0200

    Bump version: 1.0.0 → 1.0.1

--- lib/OpenServices/SNMP/Plugin/Updates.pm
+++ lib/OpenServices/SNMP/Plugin/Updates.pm
@@ -17,11 +17,11 @@ OpenServices::SNMP::Plugin::Updates - Expose pending security updates over SNMP
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 =head1 BASE OID
 
